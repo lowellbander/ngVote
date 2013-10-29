6bc268bed8070d9f5f17446a14f5416cf80e167a
Lowell Bander <lowellbander@gmail.com>
2013-10-29 14:04:58 -0700

    added in a factory, but it doesn't do anything yet. first attempt is to use GET

--- priority/static/js/services.js
+++ priority/static/js/services.js
@@ -18,6 +18,10 @@ var tastypieDataTransformer = function ($http) {
     ])
 };
 
+services.factory('Choices', function ($resource) {
+    return $resource('static/data-json/single-object.json');
+});
+
 services.factory('Task', ['$resource', '$log', '$http', 'taskURI',
     function($resource, $log, $http, taskURI) {
         return $resource(taskURI+'/api/dev/task/:taskID?format=json',
